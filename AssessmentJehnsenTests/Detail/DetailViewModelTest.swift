c2e83586955840efc73b62fea519ab0f056b2bcb
oddesa <jehnsen.hirena@sci.ui.ac.id>
2022-05-02 02:35:32 +0700

    add detailViewModelTest

--- AssessmentJehnsenTests/Detail/DetailViewModelTest.swift
+++ AssessmentJehnsenTests/Detail/DetailViewModelTest.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewModelTest.swift
+//  DetailViewModelTest.swift
 //  AssessmentJehnsenTests
 //
 //  Created by Jehnsen Hirena Kane on 02/05/22.
@@ -9,28 +9,28 @@ import Quick
 import Nimble
 @testable import AssessmentJehnsen
 
-class HomeViewModelTest: QuickSpec {
+class DetailViewModelTest: QuickSpec {
     
-    var viewModel: HomeViewModel!
+    var viewModel: DetailViewModel!
     var service: MockCoinService!
     
     override func spec() {
         describe("Test Service") {
             beforeEach {
                 self.service = MockCoinService()
-                self.viewModel = HomeViewModel(service: self.service)
+                self.viewModel = DetailViewModel(service: self.service, coinId: "bitcoin")
             }
             
-            it("Success get coin list") {
-                let response = MockResponse().coinListResponse
+            it("Success get coin detail") {
+                let response = MockResponse().coinDetailResponse
                 let data =  response.data(using: .utf8)!
                 self.service.response = try? JSONSerialization.jsonObject(
                     with: data,
                     options: []
                 )
-                self.viewModel.fetchCoins()
-                // Assert that coin succesfully fetch 15 first data
-                expect(self.viewModel.coinData.count).to(equal(15))
+                self.viewModel.getCoinDetail()
+                // Assert that coin succesfully fetch coin detail
+                expect(self.viewModel.coinDetailData).toNotEventually(beNil())
             }
         }
     }
